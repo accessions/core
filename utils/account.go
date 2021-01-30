c3e3686d3d53a769bb9d45f079651c78b6548100
zhaokebin <123456aA@2020#junmo>
2021-01-30 14:02:56 +0800

    -

--- utils/account.go
+++ utils/account.go
@@ -1,18 +1,16 @@
 package utils
 
 import (
-	"fmt"
-	"github.com/leekchan/accounting"
-	"github.com/shopspring/decimal"
-	"math/big"
+
+	//"github.com/leekchan/accounting"
+	//"github.com/shopspring/decimal"
+	//"math/big"
 )
 
-type Account struct {
-	accounting.Accounting
-}
+type Account struct {}
 
 func test ()  {
-	ac := accounting.Accounting{Symbol: "$", Precision: 2}
+	/*ac := accounting.Accounting{Symbol: "$", Precision: 2}
 	fmt.Println(ac.FormatMoney(123456789.213123))                       // "$123,456,789.21"
 	fmt.Println(ac.FormatMoney(12345678))                               // "$12,345,678.00"
 	fmt.Println(ac.FormatMoney(big.NewRat(77777777, 3)))                // "$25,925,925.67"
@@ -21,6 +19,6 @@ func test ()  {
 	fmt.Println(ac.FormatMoneyDecimal(decimal.New(123456789.213123, 0))) // "$123,456,789.21"
 
 	ac = accounting.Accounting{Symbol: "€", Precision: 2, Thousand: ".", Decimal: ","}
-	fmt.Println(ac.FormatMoney(4999.99))  // "€4.999,99"
+	fmt.Println(ac.FormatMoney(4999.99))  // "€4.999,99"*/
 
 }
