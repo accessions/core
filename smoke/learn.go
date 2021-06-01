c21965aa7c1b71df251dd3642a695bd1b9eed37a
eros <zhaokebin@pay.media>
2021-06-01 10:45:39 +0800

    -

--- smoke/learn.go
+++ smoke/learn.go
@@ -1,6 +1,6 @@
 package smoke
 
-func main()  {
+func main() {
 	// beanstalkd 分布式内存队列系统
 	// wireguard vpn内网穿透
 	// vector 日志收集工具(logstash)
@@ -11,3 +11,26 @@ func main()  {
 	// 1：如果2个微服务需要大量的相互配合，它们极有可能是相同的微服务。
 	// 2: 如果一个微服务必须依赖另一个微服务处理请求，那它就不是真正的自治
 }
+
+/**
+
+分布式链路追踪系统：
+	- https://github.com/jaegertracing/jaeger     jaeger
+
+调度系统：
+		https://airflow.apache.org/  airflow
+
+
+度量系统：
+是对软件开发项目、过程及其产品进行数据定义、收集以及分析的持续性定量化过程。
+prometheus
+
+日志系统：
+ELK
+
+MQTT 异步解耦
+
+
+VPN网络调度系统：
+wireguard
+*/
